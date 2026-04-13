0c0ce961436d31ff9f39b43466df618f9eb48ffa
hanmingaru <andytripleu@gmail.com>
2026-04-13 17:27:25 -0400

    fix path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/Hui-s-Kitchen",
   images: {
     unoptimized: true,
     minimumCacheTTL: 60,
